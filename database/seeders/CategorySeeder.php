fc64cf5e8e165dbacf5baec88a72248c1fc6d2df
Sourabh Kushwah <107801355+SourabhKushwah24@users.noreply.github.com>
2026-09-24 16:58:54 +0530

    Add seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+use Illuminate\Support\Str;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            'Mobiles',
+            'Cars',
+            'Bikes',
+            'Electronics',
+            'Furniture',
+            'Jobs',
+            'Real Estate',
+            'Services',
+        ];
+
+        foreach ($categories as $name) {
+            Category::create([
+                'name' => $name,
+                'slug' => Str::slug($name),
+            ]);
+        }
+    }
+}
